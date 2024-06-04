20c0ae8d91ca4cec87fc35b5b4485959cb347274
GXC2356 <gxcdev2356@gmail.com>
2024-06-04 16:25:30 +0300

    Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Vendor
+git clone --depth=1 https://github.com/ProjectBlaze-Devices/vendor_xiaomi_vayu.git vendor/xiaomi/vayu
+
+# Kernel (Skyline)
+git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
+
+# Gcam
+git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
+
+# Dolby
+git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
