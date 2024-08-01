f30c0a043f36943adab6d13e90590af045232023
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-08-01 16:15:52 +0300

    remove redundant repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,3 @@
-# Vendor
-rm -rf vendor/xiaomi/vayu
-git clone --depth=1 https://github.com/SkylineUI-Devices/vendor_xiaomi_vayu.git vendor/xiaomi/vayu
-
-# Kernel (Skyline)
-rm -rf kernel/xiaomi/vayu
-git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
-
 # Gcam
 rm -rf vendor/mgc
 git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
